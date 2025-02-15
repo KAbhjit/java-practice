976606f86c29c102e1441e0a0580d588cf2b4d49
KAbhijit <PKPDAS391@gmail.com>
2025-02-15 22:49:04 +0530

    Add files via upload

---
+++ Lec1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Lec1{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        int a = 10;
+        int b = 20;
+        int s = a+b;
+        System.out.println("sum is:"+s+name);
+    }
+}
